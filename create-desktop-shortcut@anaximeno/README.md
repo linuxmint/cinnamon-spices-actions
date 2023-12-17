4f01dd2abb4cac7ad41418c285f20d5150e6e87e
anaximeno <anaximenobrito@gmail.com>
2023-12-17 21:36:55 -0100

    feat: add create-desktop-shortcut@anaximeno action

---
+++ create-desktop-shortcut@anaximeno/README.md
@@ -0,0 +1,3 @@
+# Create Desktop Shortcut
+
+Creates a shortcut of folders, applications, files, and others, on the desktop folder.
---
+++ create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/action.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import pathlib
+import subprocess
+
+
+def main() -> None:
+    if len(sys.argv) <= 1:
+        print("Error: no files provided to create a desktop shortcut")
+        exit(1)
+
+    result = subprocess.run(["xdg-user-dir", "DESKTOP"], stdout=subprocess.PIPE)
+    desktop = result.stdout.decode("utf-8").replace("\n", "")
+
+    if not os.path.exists(desktop) or not os.path.isdir(desktop):
+        print("Error: XDG User Dir 'DESKTOP' not found or invalid!")
+        exit(1)
+
+    items = (pathlib.Path(item.replace("\\", "")) for item in sys.argv[1:])
+
+    for item in items:
+        # TODO: inform user of unsucessful shortcurt creations
+        try:
+            # XXX: it will not create a symlink if there's already a file with that name there,
+            #      we should instead ask the user what is the correct action when this happens.
+            if item.exists():
+                shortcut = pathlib.Path(os.path.join(desktop, item.name))
+                os.symlink(str(item.resolve()), str(shortcut.resolve()), item.is_dir())
+            else:
+                print(
+                    f"Error: couldn't create shortcut for {item.as_posix()!r}"
+                    ", not found!"
+                )
+        except:
+            print(f"Error: couldn't create shortcut for {item.as_posix()!r}!")
+
+
+if __name__ == "__main__":
+    main()
