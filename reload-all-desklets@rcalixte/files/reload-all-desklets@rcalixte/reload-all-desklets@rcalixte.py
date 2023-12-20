11f49dd65620c25ca00a32716742c6768793cd5c
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2023-12-20 07:26:55 -0500

    reload-all-desklets@rcalixte: Version 1.3.1
    Adjust Gtk.Dialog conditional for X11 and locked desklets
    Fix gtk_main_quit exception

--- reload-all-desklets@rcalixte/files/reload-all-desklets@rcalixte/reload-all-desklets@rcalixte.py
+++ reload-all-desklets@rcalixte/files/reload-all-desklets@rcalixte/reload-all-desklets@rcalixte.py
@@ -14,20 +14,21 @@ HOME: str = os.path.expanduser('~')
 gettext.bindtextdomain(f'{HOME}/.local/share/locale')
 gettext.textdomain(UUID)
 TEXT: str = gettext.gettext('Reload successfully completed.')
-OK: str = gettext.gettext('OK')
 BUS_TYPE: Gio.BusType = Gio.BusType.SESSION
 DBUS_PATH: str = 'org.Cinnamon'
 DESKLET: str = 'DESKLET'
 FLAG: Gio.DBusProxyFlags = Gio.DBusProxyFlags.NONE
 GSETTING: str = '/org/Cinnamon'
-KEY: str = 'enabled-desklets'
+ED_KEY: str = 'enabled-desklets'
+LOCKED: str = 'lock-desklets'
 SETTINGS: str = 'org.cinnamon'
 BADS: set = {'cpuload@kimse'}
+SESSION_TYPE: str = os.environ['XDG_SESSION_TYPE']
 
 
 def main():
     settings: Gio.Settings = Gio.Settings.new(SETTINGS)
-    enabled_desklets: set = {x.split(':')[0] for x in settings.get_strv(KEY)}
+    enabled_desklets: set = {x.split(':')[0] for x in settings.get_strv(ED_KEY)}
 
     try:
         proxy: Gio.DBusProxy = Gio.DBusProxy.new_for_bus_sync(BUS_TYPE, FLAG,
@@ -50,15 +51,15 @@ def main():
             except GLib.GError:
                 time.sleep(0.05)
 
-    if os.environ['XDG_SESSION_TYPE'] == 'x11':
+    if SESSION_TYPE == 'x11' and not settings.get_boolean(LOCKED):
         win = Gtk.Window(title='')
         dialog = Gtk.MessageDialog(transient_for=win, flags=0, text=TEXT,
-                                   message_type=Gtk.MessageType.INFO)
-        dialog.add_button(OK, Gtk.ResponseType.CLOSE)
-        dialog.connect('destroy', Gtk.main_quit)
+                                   message_type=Gtk.MessageType.INFO,
+                                   buttons=Gtk.ButtonsType.OK)
+        dialog.connect('destroy', lambda *_: None)
+        dialog.connect('response', lambda *_: None)
         dialog.run()
         dialog.destroy()
-        Gtk.main()
 
 
 if __name__ == '__main__':
