e17619b087ed3f7babe7d1f80daffd079af8cee3
Lee Jordan <duracell80@users.noreply.github.com>
2024-01-21 16:46:50 -0600

    Updated to include check with gsettings
    Cleaned up script, added gsettings check, better handle zenity language context and updated Readme.md

--- cinnamon-take-screenshot@duracell80/README.md
+++ cinnamon-take-screenshot@duracell80/README.md
@@ -1,16 +1,16 @@
 Take Screenshot
 ==============
 
-Launch a screenshot utility directly from the desktop context menu.
+Launch a screenshot area selection utility directly from the desktop context menu.
 
 DESCRIPTION
 -----------
 
-This action calls a screenshot utility directly from the desktop context menu.
+This action calls a screenshot utility directly from the desktop context menu to allow for the caputure of a selected area. It will auto save the capture to either the location noted from "gsettings get org.gnome.gnome-screenshot auto-save-directory" or a fallback in ~/Pictures/screenshots and display the file in pix.
 
 DEPENDENCIES
 ------------
 
 The following programs must be installed and available
 
-* `cinnamon`, `zenity` for UI and package management
+* `gnome-screenshot`, `pix`, `cinnamon`, `gsettings`, `zenity`
--- cinnamon-take-screenshot@duracell80/files/cinnamon-take-screenshot@duracell80/cinnamon-take-screenshot.sh
+++ cinnamon-take-screenshot@duracell80/files/cinnamon-take-screenshot@duracell80/cinnamon-take-screenshot.sh
@@ -4,78 +4,39 @@
 
 
 # ENVIRONMENT VARS
-LANG="en"
 DIR_PWD=$(pwd)
-DIR_TGT="${HOME}/Pictures/screenshots"
+DIR_AUTOSAVE=$(gsettings get org.gnome.gnome-screenshot auto-save-directory)
 
-if ! [ -d "${DIR_TGT}" ]; then
-    mkdir -p "${DIR_TGT}"
+if [ "${DIR_AUTOSAVE}" = "''" ]; then
+	# FALL BACK
+	DIR_TGT="${HOME}/Pictures/screenshots"
+else
+	# USERS CHOICE
+	DIR_TGT=$(echo "${DIR_AUTOSAVE}" | sed "s/'//g")
+	if ! [ -d "${DIR_TGT}" ]; then
+		mkdir -p "${DIR_TGT}"
+	fi
 fi
 
 
-
-
-# READ THE LANGUAGE FILE
-if [[ "${PWD,,}" == "${HOME}" ]]; then
+# READ THE LANGUAGE FILE FOR THE ERROR MESSAGE
+# IF SCREENSHOT APP NOT AVAILABLE, MISSING DEPS MIGHT PREVENT MENU ITEM FROM SHOWING
+if [[ "${PWD,,}" == "${HOME,,}" ]]; then
 	DIR_APP="${HOME}/.local/share/nemo/actions/cinnamon-take-screenshot@duracell80"
 else
 	DIR_APP="${PWD}"
 fi
 
+
+
+# ZENITY DOESN'T SEEM TO WORK WITHOUT THIS WHEN NOT USING ENGLISH
 LANG=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d_ -f1)
 REGI=$(locale | grep LANGUAGE | cut -d= -f2)
 if [ "${LANG}" = "" ]; then
 	LANG="en"
 fi
-
-if [ "${REGI}" = "" ]; then
-        REGI="US"
-fi
-
-# FRENCH
-if [ "${LANG,,}" = "fr" ]; then
-	export LC_ALL="fr_FR.utf-8"
-# SPANISH
-elif [ "${LANG,,}" = "es" ]; then
-        export LC_ALL="es_ES.utf-8"
-# PORTUGUESE - BRAZIL
-elif [ "${REGI,,}" = "pt_br" ]; then
-        export LC_ALL="pt_BR.utf-8"
-	    LANG="pt"
-# PORTUGUESE
-elif [ "${LANG,,}" = "pt" ]; then
-        export LC_ALL="pt_PT.utf-8"        
-# GERMAN
-elif [ "${LANG,,}" = "de" ]; then
-        export LC_ALL="de_DE.utf-8"
-# ITALIAN
-elif [ "${LANG,,}" = "it" ]; then
-        export LC_ALL="it_IT.utf-8"
-# DANISH
-elif [ "${LANG,,}" = "da" ]; then
-        export LC_ALL="da_DK.utf-8"
-# FINNISH
-elif [ "${LANG,,}" = "fi" ]; then
-        export LC_ALL="fi_FI.utf-8"
-# NORWEGIAN NYNORSK
-elif [ "${LANG,,}" = "nn" ]; then
-        export LC_ALL="nn_NO.utf-8"
-        LANG_INS="installed"
-# HUNGARIAN
-elif [ "${LANG,,}" = "hu" ]; then
-        export LC_ALL="hu_HU.utf-8"
-# TURKISH
-elif [ "${LANG,,}" = "tr" ]; then
-        export LC_ALL="tr_TR.utf-8"
-# RUSSIAN
-elif [ "${LANG,,}" = "ru" ]; then
-        export LC_ALL="ru_RU.utf-8"
-# ENGLISH
-elif [ "${LANG,,}" = "en" ]; then
-    export LC_ALL="en_GB.utf-8"
-else
-	export LC_ALL="en_US.utf-8"
-fi
+export LC_ALL="${REGI}.utf-8"
+# END ZENITY
 
 
 if [ -f "${DIR_APP}/po-sh/lang_${LANG,,}.txt" ]; then
@@ -86,7 +47,7 @@ if [ -f "${DIR_APP}/po-sh/lang_${LANG,,}.txt" ]; then
 		declare $suffix="${col[1]}"
 	done < "${DIR_APP}/po-sh/lang_${LANG,,}.txt"
 else
-	#XT-EN
+	#FALL BACK ON EN
 	LAN00="Screenshot app not available, exiting."
 fi
 
@@ -94,16 +55,21 @@ fi
 
 
 
+
+
 # IF GNOME SCREEN SHOT AVAILABLE - TODO SUPPORT OTHER NON MINT ENVIRONMENTS
-# OFFER THE AREA SELECTION TOOL, SAVE DIRECTLY TO PICTURES AND OPEN IN PIX
+# OFFER THE AREA SELECTION TOOL, SAVE DIRECTLY TO DIRECTORY AND OPEN IN PIX
 if [[ $(compgen -c | grep -iw 'gnome-screenshot' | head -n1 | wc -l) == "0" ]]; then
     zenity --error --icon-name=security-high-symbolic --text="${LAN00}";
 else
+    # US Date format: 2024-01-21 16-27-09 = +%Y-%m-%d %H:%M
+    # ELSE USE UNIVERSAL SECONDS FOR FILE NAME
     TS=$(date +%s)
-    gnome-screenshot --area --file="${DIR_TGT}/screenshot_${TS}.png"
+
+   gnome-screenshot --area --file="${DIR_TGT}/screenshot-auto_${TS}.png"
     if [[ $(compgen -c | grep -iw 'pix' | head -n1 | wc -l) == "0" ]]; then
         exit
     else
-        pix "${DIR_TGT}/screenshot_${TS}.png" &
+        pix "${DIR_TGT}/screenshot-auto_${TS}.png" &
     fi
 fi
