786139d5d847243aa9b6b3b6a6bda067513256bb
yfabrik <138105766+yfabrik@users.noreply.github.com>
2024-01-24 16:57:35 +0100

    bugfix path with space

--- quick-desktop-entry@yfabrik/files/quick-desktop-entry@yfabrik/gen-desktop-entry@yfabrik.sh
+++ quick-desktop-entry@yfabrik/files/quick-desktop-entry@yfabrik/gen-desktop-entry@yfabrik.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
-file=$1
-dirpath=$2
-filepath=$3
-filename=$4
-desktopfile=$HOME/.local/share/applications/$filename.desktop
+filepath="${1}"
+parentpath="${2}"
+filename="${filepath:${#parentpath}+1}"
 
+desktopfile=$HOME/.local/share/applications/"${filename// /_}".desktop
+filemime=$(xdg-mime query filetype "${filepath}")
+mimedefault=$(xdg-mime query default "${filemime}")
 
-case $(xdg-mime query filetype $file) in
+if [[ -n "${mimedefault}" ]]; then
 
-application/vnd.appimage)
-    dirpath="$HOME/Applications"
-    mkdir -p ${dirpath}
-    chmod +x $file
-    mv $file ${dirpath}
-    filepath=$HOME/Applications/$file
-    command=$filepath
-    ;;
-application/x-ms-dos-executable)
-    command="wine $filepath"
-    ;;
-application/*)
-    command=$filepath
-    ;;
-*)
-    command="xdg-open $filepath"
-    ;;
+	command="xdg-open \"${filepath}\""
+	
+else
 
-esac
+	if [[ "${filemime}" == "application/vnd.appimage" ]]; then
 
+		parentpath="$HOME/Applications"
+		mkdir -p "${parentpath}"
+		chmod +x "${filepath}"
+		mv "${filepath}" "${parentpath}"
+		filepath="${parentpath}/${filename}"
+		#TODO extract icon
+		command="${filepath}"
+	else
+		command="${filepath}"
+	fi
+fi
 
-cat >$desktopfile <<EOF
+cat >"${desktopfile}" <<EOF
 [Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
-Exec=$command
-Name=$filename
-Comment=$filename
-Icon=$filename
-Path=$dirpath
+Exec=${command}
+Name=${filename}
+Comment=${filename}
+Path=${parentpath}
+#Icon=
 #StartupNotify=true
 #Categories=#Utility;Development;Office;Graphics;Network;AudioVideo;System;Settings;Education;Game;Science;Accessibility
 ## if icon not working in the taskbar get from ' xprop WM_CLASS '
 #StartupWMClass=glfw-application
 #Keyword=
 #MimeType=
-
+ 
 # Desktop Action Open
 # Name=Open
 # Exec=/path/to/open-command
 # Icon=open-icon
 # Tooltip=Open the application
-
+ 
 EOF
-chmod +x $desktopfile
-xdg-open $desktopfile
+
+chmod +x "${desktopfile}"
+xdg-open "${desktopfile}"
