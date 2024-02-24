fd9ef12292b9e50aae27358e65383f49bbef2621
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-02-24 08:51:46 -0100

    create-desktop-shortcut@anaximeno: Add message dialogs (#218)

--- create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/action.py
+++ create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/action.py
@@ -2,41 +2,95 @@
 
 import os
 import sys
-import pathlib
 import subprocess
+
+from pathlib import Path
+
+import ui
+import text
+
+
+DEBUG = os.environ.get("NEMO_DEBUG", False) == "Actions"
+def log(*args, **kwargs):
+    if DEBUG: print(f"Action {text.UUID}:", *args, **kwargs)
+
+
+def send_to_trash(item: Path) -> bool:
+    try:
+        response = subprocess.run(
+            ["gio", "trash", item.as_posix()],
+            stderr=subprocess.DEVNULL,
+        )
+        return response.returncode == 0
+    except:
+        return False
+
+
+def prompt_not_created_message() -> None:
+    dialog = ui.BasicMessageDialogWindow()
+    dialog.run()
+    dialog.destroy()
+
+
+def prompt_override_permission() -> str:
+    dialog = ui.OverrideQuestionMessageDialogWindow()
+    override = dialog.run()
+    dialog.destroy()
+    return override
+
+
+override = None
+def link_shortcut_to_item(shortcut: Path, item: Path) -> bool:
+    global override
+    if shortcut.exists():
+        if override is None:
+            override = prompt_override_permission()
+
+        if override == "y":
+            send_to_trash(shortcut)
+
+        if shortcut.exists():
+            log(
+                f"Info: not creating a shortcut for {item.name!r}, "
+                "item with the same name in the desktop folder."
+            )
+            return False
+
+    if item.exists():
+        shortcut.symlink_to(item.resolve(), target_is_directory=item.is_dir())
+        return True
+    else:
+        log(f"Error: couldn't create shortcut for {item.name!r}" ", not found!")
+        return False
 
 
 def main() -> None:
     if len(sys.argv) <= 1:
-        print("Error: no files provided to create a desktop shortcut")
+        log("Error: no files provided to create a desktop shortcut")
         exit(1)
 
     result = subprocess.run(["xdg-user-dir", "DESKTOP"], stdout=subprocess.PIPE)
     desktop = result.stdout.decode("utf-8").replace("\n", "")
 
     if not os.path.exists(desktop) or not os.path.isdir(desktop):
-        print("Error: XDG User Dir 'DESKTOP' not found or invalid!")
+        log("Error: XDG User Dir 'DESKTOP' not found or invalid!")
         exit(1)
 
-    items = (pathlib.Path(item.replace("\\", "")) for item in sys.argv[1:])
+    items = (Path(item.replace("\\ ", " ")) for item in sys.argv[1:])
 
+    not_created = []
     for item in items:
-        # XXX: it will not create a symlink if there's already a file with that name there,
-        # we should instead ask the user what is the correct action (replace, replace all, cancel, etc)
-        # when this happens.
         try:
-            if item.exists():
-                shortcut = pathlib.Path(os.path.join(desktop, item.name))
-                shortcut.symlink_to(item.resolve(), target_is_directory=item.is_dir())
-            else:
-                print(
-                    f"Error: couldn't create shortcut for {item.as_posix()!r}"
-                    ", not found!"
-                )
-        except:
-            print(f"Error: couldn't create shortcut for {item.as_posix()!r}")
+            shortcut = Path(os.path.join(desktop, item.name))
+            created = link_shortcut_to_item(shortcut=shortcut, item=item)
+            if not created:
+                not_created.append(item)
+        except Exception as e:
+            not_created.append(item)
+            log(f"Error: couldn't create shortcut for {item.name!r}, exception => {e}")
 
-    # TODO: inform user of unsucessful shortcurt creations
+    if any(not_created):
+        prompt_not_created_message()
 
 
 if __name__ == "__main__":
