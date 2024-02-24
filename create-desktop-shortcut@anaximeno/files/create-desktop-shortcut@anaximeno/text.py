5c7cac574f42dde65e6f52fecbdcaf52b4ec3bdf
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-02-24 05:01:11 -0100

    chore: Update some texts

--- create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/text.py
+++ create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/text.py
@@ -11,7 +11,10 @@ def _(msg: str) -> str: return gettext.gettext(msg)
 
 
 ACTION_TITLE = _("Create Desktop Shortcut Action")
+
 SHORTCUTS_NOT_CREATED_MESSAGE = _("Couldn't create all shortcuts!")
+
 FILE_ALREADY_EXISTS_AT_THE_DESKTOP_FOLDER = _(
-    "File named '%s' already exists inside the desktop folder!"
-    " What should be done?")
+    "Some items already exist with the same name in the desktop folder, "
+    "should they be overridden? (Overridden items in the desktop folder will be sent to the trash.)"
+)
