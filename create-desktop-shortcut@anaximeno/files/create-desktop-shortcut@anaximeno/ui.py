a5c4eb9c3d3287afa81a95239dee7d42ecc3e7c2
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-02-24 05:00:46 -0100

    refactor: directly use default text and other refactoring

--- create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/ui.py
+++ create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/ui.py
@@ -1,21 +1,21 @@
+import text
 import gi
 
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gio, Gtk
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
 
 class BasicMessageDialogWindow(Gtk.Window):
-    def __init__(self, title: str, message: str) -> None:
+    def __init__(self) -> None:
         super().__init__(title="")
-        self.title = title
-        self.message = message
         self.dialog = Gtk.MessageDialog(
             flags=0,
             transient_for=self,
             message_type=Gtk.MessageType.INFO,
             buttons=Gtk.ButtonsType.OK,
-            text=self.title)
-        self.dialog.format_secondary_text(self.message)
+            text=text.ACTION_TITLE,
+        )
+        self.dialog.format_secondary_text(text.SHORTCUTS_NOT_CREATED_MESSAGE)
 
     def run(self):
         return self.dialog.run()
@@ -26,22 +26,23 @@ class BasicMessageDialogWindow(Gtk.Window):
 
 
 class OverrideQuestionMessageDialogWindow(Gtk.Window):
-    # TODO: finish this
-    def __init__(self, title: str, message: str) -> None:
+    def __init__(self) -> None:
         super().__init__(title="")
-        self.title = title
-        self.message = message
         self.dialog = Gtk.MessageDialog(
             flags=0,
             transient_for=self,
             message_type=Gtk.MessageType.QUESTION,
             buttons=Gtk.ButtonsType.YES_NO,
-            text=self.title)
+            text=text.ACTION_TITLE,
+        )
         self.dialog.set_default_size(150, 100)
-        self.dialog.format_secondary_text(self.message)
+        self.dialog.format_secondary_text(
+            text.FILE_ALREADY_EXISTS_AT_THE_DESKTOP_FOLDER
+        )
 
     def run(self):
-        return self.dialog.run()
+        response = self.dialog.run()
+        return "y" if response == Gtk.ResponseType.YES else "n"
 
     def destroy(self):
         self.dialog.destroy()
