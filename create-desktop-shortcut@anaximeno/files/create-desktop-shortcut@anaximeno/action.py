b453be9de94d4987dce7af632c319781e6659943
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-02-24 23:38:47 -0100

    create-desktop-shortcut@anaximeno: Add installation instructions (#227)
    * docs: Add installation instructions
    
    * chore: Dont set the default value for the NEMO_DEBUG env get
    
    * docs: Fix typo

--- create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/action.py
+++ create-desktop-shortcut@anaximeno/files/create-desktop-shortcut@anaximeno/action.py
@@ -10,7 +10,7 @@ import ui
 import text
 
 
-DEBUG = os.environ.get("NEMO_DEBUG", False) == "Actions"
+DEBUG = os.environ.get("NEMO_DEBUG") == "Actions"
 def log(*args, **kwargs):
     if DEBUG: print(f"Action {text.UUID}:", *args, **kwargs)
 
