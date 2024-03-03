fda8a0b372957d3fcea899128d0ba2f337792a87
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2024-03-03 05:40:24 -0500

    add translation support

--- xkill@eson57/files/xkill@eson57/xkill@eson57.sh
+++ xkill@eson57/files/xkill@eson57/xkill@eson57.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
-if [ ! -e ~/.local/share/nemo/actions/xkill@eson57/.killdone ]
+#!/usr/bin/bash
+
+TEXTDOMAIN="xkill@eson57"
+TEXTDOMAINDIR="${HOME}/.local/share/locale"
+_TEXT=$"Click on the application that you want to kill."
+TEXT="$(/usr/bin/gettext "$_TEXT")"
+_ONCE=$"This instruction for XKill will not be shown again."
+ONCE="$(/usr/bin/gettext "$_ONCE")"
+
+KILLDONE=~/.local/share/nemo/actions/xkill@eson57/.killdone
+
+if [ ! -e $KILLDONE ]
 then
-zenity --info --title="XKill" --text="Click on the application that you want to kill."
-echo "This instruction for XKill will not be shown again." > ~/.local/share/nemo/actions/xkill@eson57/.killdone
+zenity --info --title="XKill" --text="${TEXT}"
+echo "${ONCE}" > $KILLDONE
 fi
 sleep 1
 xkill
