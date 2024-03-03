03bcfd2c0e177b46c920bddc5433c658f7ffadcc
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-03-03 16:22:55 -0100

    chore: Improve behaviour if file exists

--- paste-into-file@anaximeno/files/paste-into-file@anaximeno/action.py
+++ paste-into-file@anaximeno/files/paste-into-file@anaximeno/action.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os, sys
+import os, sys, pathlib
 import subprocess
 import aui
 import text
@@ -14,6 +14,28 @@ def get_append_to_file_perm(filename) -> bool:
     response = window.run()
     window.destroy()
     return response == aui.QuestionDialogWindow.RESPONSE_YES
+
+
+def get_file_name(default_filename: str = None) -> str:
+    window = aui.EntryDialogWindow(
+        title=text.ACTION_TITLE,
+        window_icon_path=aui.get_action_icon_path(text.UUID),
+        label=text.ENTRY_LABEL,
+        default_text=default_filename,
+    )
+    response = window.run()
+    window.destroy()
+    return response
+
+
+def prompt_invalid_file_name() -> None:
+    window = aui.InfoDialogWindow(
+        title=text.ACTION_TITLE,
+        message=text.INVALID_FILE_NAME,
+        window_icon_path=aui.get_action_icon_path(text.UUID),
+    )
+    window.run()
+    window.destroy()
 
 
 def main() -> None:
@@ -35,13 +57,7 @@ def main() -> None:
     ).stdout.decode("utf-8")
 
     if filename is None:
-        window = aui.EntryDialogWindow(
-            title=text.ACTION_TITLE,
-            window_icon_path=aui.get_action_icon_path(text.UUID),
-            label=text.ENTRY_LABEL,
-        )
-        response = window.run()
-        window.destroy()
+        response = get_file_name()
 
         if response is not None and response.strip() != "":
             filename = os.path.join(directory, response)
@@ -51,17 +67,23 @@ def main() -> None:
                 file.write(clipcontent)
         else:
             if response is not None:
-                window = aui.InfoDialogWindow(
-                    title=text.ACTION_TITLE,
-                    message=text.INVALID_FILE_NAME,
-                    window_icon_path=aui.get_action_icon_path(text.UUID),
-                )
-                window.run()
-                window.destroy()
+                prompt_invalid_file_name()
             exit(1)
     else:
-        if os.path.exists(filename) and not get_append_to_file_perm(filename):
-            exit(1)
+        if os.path.exists(filename):
+            response = get_file_name(default_filename=pathlib.Path(filename).name)
+
+            if response is None:
+                exit(1)
+
+            if response.strip() == "":
+                prompt_invalid_file_name()
+                exit(1)
+
+            filename = os.path.join(directory, response)
+            if os.path.exists(filename) and not get_append_to_file_perm(filename):
+                exit(1)
+
         with open(filename, "at") as file:
             file.write(clipcontent)
 
