72f0bef7fd1466dc45efbbf2c818f85a403934a5
Åke Engelbrektson <eson57@users.noreply.github.com>
2024-03-03 09:33:03 +0100

    Update xkill@eson57.sh
    Move .killdone file inside app

--- xkill@eson57/files/xkill@eson57/xkill@eson57.sh
+++ xkill@eson57/files/xkill@eson57/xkill@eson57.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ ! -e ~/.killdone ]
+if [ ! -e ~/.local/share/nemo/actions/xkill@eson57/.killdone ]
 then
 zenity --info --title="XKill" --text="Click on the application that you want to kill."
-echo "This instruction for XKill will not be shown again." > ~/.killdone
+echo "This instruction for XKill will not be shown again." > ~/.local/share/nemo/actions/xkill@eson57/.killdone
 fi
 sleep 1
 xkill
