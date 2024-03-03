f17d929ee25fb921bf05c5b53a816d3dad04a8e6
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-03-03 01:55:00 -0100

    fix: Guarantee translation of text const

--- paste-into-file@anaximeno/files/paste-into-file@anaximeno/text.py
+++ paste-into-file@anaximeno/files/paste-into-file@anaximeno/text.py
@@ -14,7 +14,7 @@ ACTION_TITLE = _("Paste into file")
 
 ENTRY_LABEL = _("File name:")
 
-FILE_EXISTS = (
+FILE_EXISTS = _(
     "File %s already exist inside the current directory. Do you want "
     "to append the clipboard content to the end of the file?"
 )
