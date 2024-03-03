c930d73d7cbf97f749d972ddaadb25170070733b
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-03-03 17:30:59 -0100

    Add paste-into-file@anaximeno (#241)

---
+++ paste-into-file@anaximeno/files/paste-into-file@anaximeno/check.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import os, sys
+import subprocess
+
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        exit(1)
+
+    directory = sys.argv[1].replace("\\ ", " ")
+    files = sys.argv[2:]
+
+    # if dir does not exist or is not writable
+    if not os.path.exists(directory) or not os.access(directory, os.W_OK):
+        exit(1)
+
+    # Max. one file is accepted
+    if len(files) > 1:
+        exit(1)
+
+    # Check write perms if file given
+    if len(files) == 1 and not os.access(files[0].replace("\\ ", " "), os.W_OK):
+        exit(1)
+
+    clipcontent = subprocess.run(
+        ["xclip", "-out", "-selection", "clipboard"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ).stdout.decode("utf-8")
+
+    # Check if the clipboard is not empty
+    if clipcontent.strip() == "":
+        exit(1)
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
