d87d2f66ed7e876902e16927644038ae57494ff7
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-03-04 00:43:37 -0100

    feat: Add algorithm to formalize given git address

--- clone-git-repo@anaximeno/files/clone-git-repo@anaximeno/action.py
+++ clone-git-repo@anaximeno/files/clone-git-repo@anaximeno/action.py
@@ -6,6 +6,11 @@ import aui
 import text
 
 
+REPO_NAME_REGEX = r"\/([^\/]+)(\.git)?$"
+
+ASSUME_PROTOCOL = "https"
+
+
 def get_repository_address() -> str | None:
     window = aui.EntryDialogWindow(
         title=text.ACTION_TITLE,
@@ -30,21 +35,29 @@ def get_name_to_clone_as(default_name: str = None) -> str | None:
 
 
 def get_repo_name_from_address(address: str) -> str:
-    ADDRESS_REGEX = r"\/([^\/]+)(\.git)?$"
-    re_match = re.search(ADDRESS_REGEX, address)
+    re_match = re.search(REPO_NAME_REGEX, address)
     if re_match is not None:
         return re_match.group(1).replace(".git", "")
     return ""
 
 
-def prompt_git_address_invalid(address: str) ->  bool:
+def prompt_git_address_invalid(address: str) -> bool:
     window = aui.InfoDialogWindow(
         title=text.ACTION_TITLE,
-        message=text.ADDRESS_INVALID % (address, "https://to-be-added.url"),
+        message=text.ADDRESS_INVALID
+        % (address, "https://to-be-added.url"),  # XXX: update url
         window_icon_path=aui.get_action_icon_path(text.UUID),
     )
     window.run()
     window.destroy()
+
+
+def formalize_address(address: str) -> str:
+    if not address.startswith("git@") and not "://" in address:
+        address = f"{ASSUME_PROTOCOL}://{address}"
+    if not address.endswith(".git"):
+        address = f"{address}.git"
+    return address
 
 
 def main() -> None:
@@ -57,6 +70,7 @@ def main() -> None:
         prompt_git_address_invalid(address)
         exit(1)
 
+    formal_address = formalize_address(address)
     folder_name = get_name_to_clone_as(repo_name)
 
 
