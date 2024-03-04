9353a838e13766adc3072b9171bdf9d2bc03bd56
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-03-04 17:24:09 -0100

    chore: Update text and translations

--- paste-into-file@anaximeno/files/paste-into-file@anaximeno/text.py
+++ paste-into-file@anaximeno/files/paste-into-file@anaximeno/text.py
@@ -20,12 +20,11 @@ FILE_EXISTS = _(
 
 INVALID_FILE_NAME = _("Invalid file name!")
 
-NO_CLIPBOARD_CONTENT = _(
-    "There's nothing on the clipboard to paste into a file! Aborting operation..."
-)
+NO_CLIPBOARD_CONTENT = _("No content found in clipboard. Operation cancelled.")
+
 # NOTE: these below aren't used yet, but might need them in a future release
-APPEND_TO_END = _("Append to the end of the file")
+APPEND_TO_END = _("Paste to the end of the file.")
 
-OVERRIDE_CONTENT = _("Override the content of the file")
+OVERRIDE_CONTENT = _("Replace the content of the file.")
 
-FILE_EXISTS = _("File '%s' already exists. How to proceed?")
+STATE_THAT_FILE_EXISTS = _("File '%s' already exists.")
