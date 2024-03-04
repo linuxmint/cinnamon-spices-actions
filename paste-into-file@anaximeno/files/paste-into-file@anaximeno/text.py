a2a24787887fb7875a1ff76813b4321779d8ff04
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-03-04 22:09:46 -0100

    paste-into-file@anaximeno: Allow to choose action if file exists (#258)
    * chore: Update AUI lib version to 0.2
    
    * chore: Update pt translation
    
    * feat: Allow usr to choose the best action is file exists
    
    * chore: Remove no more necessary translation text
    
    * chore: Bump version
    
    * chore: Small update to the text
    
    * chore: Update default paste fn param

--- paste-into-file@anaximeno/files/paste-into-file@anaximeno/text.py
+++ paste-into-file@anaximeno/files/paste-into-file@anaximeno/text.py
@@ -14,17 +14,13 @@ ACTION_TITLE = _("Paste into file")
 
 ENTRY_LABEL = _("File name:")
 
-FILE_EXISTS = _(
-    "File '%s' already exists. Do you want to append the clipboard content to the end of the file?"
-)
-
 INVALID_FILE_NAME = _("Invalid file name!")
 
 NO_CLIPBOARD_CONTENT = _("No content found in clipboard. Operation cancelled.")
 
 # NOTE: these below aren't used yet, but might need them in a future release
-APPEND_TO_END = _("Paste to the end of the file.")
+APPEND_TO_END = _("Paste to the end of the file")
 
-OVERRIDE_CONTENT = _("Replace the content of the file.")
+OVERRIDE_CONTENT = _("Replace the content of the file")
 
 STATE_THAT_FILE_EXISTS = _("File '%s' already exists.")
