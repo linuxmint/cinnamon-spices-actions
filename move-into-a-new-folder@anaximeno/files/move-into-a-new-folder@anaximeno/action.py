338986b3e2e9f9ae205711718b7ea5cc52b508fe
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-04-27 21:19:03 -0100

    move-into-a-new-folder@anaximeno: Version 1.2.0 (#346)
    * chore: Update AUI to v0.4
    
    * refactor: Move to a class notation
    
    * chore: Bump version

--- move-into-a-new-folder@anaximeno/files/move-into-a-new-folder@anaximeno/action.py
+++ move-into-a-new-folder@anaximeno/files/move-into-a-new-folder@anaximeno/action.py
@@ -8,64 +8,50 @@ import aui
 from pathlib import Path
 
 
-def get_new_folder_path(base_folder: Path) -> Path:
-    window = aui.EntryDialogWindow(
-        title=text.ACTION_TITLE,
-        label=text.ENTRY_LABEL,
-        default_text=text.ENTRY_DEFAULT,
-        window_icon_path=aui.get_action_icon_path(text.UUID),
-    )
-    name = window.run()
-    window.destroy()
-    path = base_folder.joinpath(name)
-    return name, path
+class MoveIntoNewFolder:
+    def __init__(self, base_folder: Path, items: list) -> None:
+        self._window_icon = aui.get_action_icon_path(text.UUID)
+        self._base_folder = base_folder
+        self._items = items
 
-
-def move_items_to_the_new_folder(items: list[str], folder: Path) -> tuple[list, list]:
-    moved, not_moved = [], []
-    for item in items:
-        item_path = Path(item.replace("\\", ""))
-
-        if not item_path.exists():
-            continue
-
-        try:
-            new_path = folder.joinpath(item_path.name)
-            item_new_path = item_path.rename(new_path.resolve())
-            if item_new_path.exists() and not item_path.exists():
-                moved.append(item_new_path)
-            else:
-                not_moved.append(item_path)
-        except:
-            not_moved.append(item_path)
-
-    return moved, not_moved
-
-
-def main():
-    base_folder = Path(sys.argv[1].replace("\\", ""))
-    new_folder_name, new_folder_path = get_new_folder_path(base_folder)
-
-    if not new_folder_name.strip():
-        exit(1)
-
-    if new_folder_path.exists():
-        window = aui.QuestionDialogWindow(
-            message=text.FOLDER_EXISTS % new_folder_path.name,
+    def get_new_folder_path(self) -> tuple[str, Path]:
+        window = aui.EntryDialogWindow(
             title=text.ACTION_TITLE,
-            window_icon_path=aui.get_action_icon_path(text.UUID),
+            label=text.ENTRY_LABEL,
+            window_icon_path=self._window_icon,
+            default_text=text.ENTRY_DEFAULT,
         )
-        response = window.run()
+        name = window.run()
         window.destroy()
-
-        if response != aui.QuestionDialogWindow.RESPONSE_YES:
+        if not name.strip():
             exit(1)
-    else:
+        path = self._base_folder.joinpath(name)
+        return name, path
+
+
+    def move_items_to_folder(self, items: list[str], folder: Path) -> tuple[list, list]:
+        moved, not_moved = [], []
+        for item in items:
+            item_path = Path(item.replace("\\", ""))
+            if not item_path.exists():
+                continue
+            try:
+                new_path = folder.joinpath(item_path.name)
+                item_at_new_path = item_path.rename(new_path.resolve())
+                if item_at_new_path.exists() and not item_path.exists():
+                    moved.append(item_at_new_path)
+                else:
+                    not_moved.append(item_path)
+            except:
+                not_moved.append(item_path)
+        return moved, not_moved
+
+    def try_create_folder(self, path: Path) -> bool:
         try:
-            os.mkdir(new_folder_path)
+            os.mkdir(path)
         except:
             window = aui.InfoDialogWindow(
-                message=text.FOLDER_NOT_CREATED % new_folder_path.name,
+                message=text.FOLDER_NOT_CREATED % path.name,
                 title=text.ACTION_TITLE,
                 window_icon_path=aui.get_action_icon_path(text.UUID),
             )
@@ -73,19 +59,39 @@ def main():
             window.destroy()
             exit(1)
 
-    moved, not_moved = move_items_to_the_new_folder(sys.argv[2:], new_folder_path)
+    def run(self):
+        new_folder_name, new_folder_path = self.get_new_folder_path()
 
-    if not_moved:
-        message = text.ALL_NOT_MOVED if not moved else text.N_NOT_MOVED % len(not_moved)
-        window = aui.InfoDialogWindow(
-            message=message,
-            window_icon_path=aui.get_action_icon_path(text.UUID),
-            title=text.ACTION_TITLE,
-        )
-        window.run()
-        window.destroy()
-        exit(1)
+        if new_folder_path.exists():
+            window = aui.QuestionDialogWindow(
+                message=text.FOLDER_EXISTS % new_folder_name,
+                title=text.ACTION_TITLE,
+                window_icon_path=aui.get_action_icon_path(text.UUID),
+            )
+            response = window.run()
+            window.destroy()
+
+            if response != aui.QuestionDialogWindow.RESPONSE_YES:
+                exit(1)
+        else:
+            self.try_create_folder(new_folder_path)
+
+        moved, not_moved = self.move_items_to_folder(self._items, new_folder_path)
+
+        if not_moved:
+            message = text.ALL_NOT_MOVED if not moved else text.N_NOT_MOVED % len(not_moved)
+            window = aui.InfoDialogWindow(
+                message=message,
+                window_icon_path=aui.get_action_icon_path(text.UUID),
+                title=text.ACTION_TITLE,
+            )
+            window.run()
+            window.destroy()
+            exit(1)
 
 
 if __name__ == "__main__":
-    main()
+    base_folder = Path(sys.argv[1].replace("\\", ""))
+    items = sys.argv[2:]
+    action = MoveIntoNewFolder(base_folder, items)
+    action.run()
