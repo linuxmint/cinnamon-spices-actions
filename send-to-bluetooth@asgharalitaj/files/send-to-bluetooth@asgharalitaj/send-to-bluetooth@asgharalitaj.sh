7f5ae06feef40383266298fc2d603c5ed209c8be
Asghar Ali Taj <109938513+AsgharAliTaj@users.noreply.github.com>
2024-07-19 09:42:10 +0500

    Update send-to-bluetooth@asgharalitaj.sh

--- send-to-bluetooth@asgharalitaj/files/send-to-bluetooth@asgharalitaj/send-to-bluetooth@asgharalitaj.sh
+++ send-to-bluetooth@asgharalitaj/files/send-to-bluetooth@asgharalitaj/send-to-bluetooth@asgharalitaj.sh
@@ -9,8 +9,4 @@ fi
 # File to send
 FILE="$1"
 
-# Choose Bluetooth device
-DEVICE=$(blueman-sendto --choose)
-
-# Send file to the chosen device
-blueman-sendto --send $FILE $DEVICE
+blueman-sendto $FILE
