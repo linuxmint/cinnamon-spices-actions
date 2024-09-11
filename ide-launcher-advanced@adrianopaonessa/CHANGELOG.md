4cb8f56a58db086d65f3200009f833d138f0b4ec
Adriano Paonessa <adrianopaonessa20@gmail.com>
2024-09-11 14:44:37 +0200

    FEAT italian translation and path-checker.sh update

--- ide-launcher-advanced@adrianopaonessa/CHANGELOG.md
+++ ide-launcher-advanced@adrianopaonessa/CHANGELOG.md
@@ -1,3 +1,7 @@
+### 1.0.1
+
+* New italian translation
+
 ### 1.0.0
 
 * Initial release
--- ide-launcher-advanced@adrianopaonessa/files/ide-launcher-advanced@adrianopaonessa/path-checker.sh
+++ ide-launcher-advanced@adrianopaonessa/files/ide-launcher-advanced@adrianopaonessa/path-checker.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Check for exceptions
-CURRENT_DIR=$1
+## Here we handle exception.
+## We will catch if currently right clicked directory actually exists,
+## ... Or is it the root filesystem?
+WORKING_DIR=$1
 
 
-# if CURRENT_DIR is empty or CURRENT_DIR is root
-if [[ -z "$CURRENT_DIR" || "$CURRENT_DIR" == '/' ]]; then
-    # don't show the action
+## IF:   WORKING_DIR is empty      (eg. recents, trash or search results)   --- does not exist
+## OR:   WORKING_DIR is /          (eg. favorites or root filesystem)       --- we don't touch root filesystem
+if [[ -z "$WORKING_DIR" || "$WORKING_DIR" == '/' ]]; then
+    
+    # THEN: We exit with err. code 1, so that Nemo action will receive err. condition
+    # ...and won't continue executing, aka. won't show VSCode menu entry at all.
     exit 1
 fi
 
-# everything is fine, show the action
 exit 0
