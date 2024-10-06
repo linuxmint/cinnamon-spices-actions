0a8f3aa95bbceb00b52aaa8abf71e9cf96916cf0
anaximeno <anaximenobrito@gmail.com>
2024-10-06 03:14:34 -0100

    Add some of the UI

--- install-fonts@anaximeno/files/install-fonts@anaximeno/action.py
+++ install-fonts@anaximeno/files/install-fonts@anaximeno/action.py
@@ -42,10 +42,13 @@ def symlink_dir(link_dir, target_dir) -> None:
         log(f"Created symlink: {link_dir} -> {target_dir}")
 
 
-class FontsInstallerAction:
+class InstallFontsAction:
     def __init__(self, file_paths: list[Path]) -> None:
         self.file_paths = file_paths
         self.fonts_dir = self.get_fonts_dir()
+        self.window_icon_path = aui.get_action_icon_path(text.UUID)
+        self._font_cache_upd_proc = None
+        self._font_cache_upd_proc_cancelled = False
 
     def get_fonts_dir(self) -> str | None:
         fonts_dir = None
@@ -67,8 +70,22 @@ class FontsInstallerAction:
         return fonts_dir
 
     def update_fonts_cache(self) -> bool:
+        def handle_progress(user_data, window: aui.ProgressbarDialogWindow):
+            if (
+                self._font_cache_upd_proc
+                and self._font_cache_upd_proc.poll() is not None
+            ):
+                window.stop()
+                window.destroy()
+            else:
+                window.progressbar.pulse()
+
+        def handle_cancel():
+            self._font_cache_upd_proc_cancelled = True
+            self._font_cache_upd_proc.kill()
+
         try:
-            subprocess.run(
+            self._font_cache_upd_proc = subprocess.Popen(
                 ["fc-cache", "-f", "-v"],
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL,
@@ -76,9 +93,22 @@ class FontsInstallerAction:
         except Exception as e:
             log(f"Could not create font cache: {e}")
             return False
-        return True
 
-    def install_font_to_dir(self, file: Path, dir: str) -> bool:
+        window = aui.ProgressbarDialogWindow(
+            title=text.WINDOW_TITLE,
+            message=text.CACHE_UPDATE_PROGRESS_MESSAGE,
+            window_icon_path=self.window_icon_path,
+            timeout_callback=handle_progress,
+            on_cancel_callback=handle_cancel,
+            timeout_ms=35,
+        )
+
+        window.run()
+        window.destroy()
+
+        return not self._font_cache_upd_proc_cancelled
+
+    def install_font_at_dir(self, file: Path, dir: str) -> bool:
         try:
             if not file.exists():
                 log(f"Error: File not found at: {file.resolve()}")
@@ -102,22 +132,33 @@ class FontsInstallerAction:
             return False
         return True
 
-    def finish(self, n_files_moved: int) -> None:
-        if n_files_moved == 0:
-            log("Fonts were not installed!")
-            exit(1)
-
-        font_cache_success = self.update_fonts_cache()  # Add progress?
-
-        if not font_cache_success:
-            log(
-                "Font cache could be updated with success!"
-            )  # XXX: add this to the second line on the ui
-
+    def finish(self, n_files_moved: int, font_cache_success: bool) -> None:
         if n_files_moved < len(self.file_paths):
             log("Some fonts could not be installed successfuly!")
+            window = aui.InfoDialogWindow(
+                title=text.WINDOW_TITLE,
+                message=text.PARTIAL_SUCCESS_INSTALL,
+                window_icon_path=self.window_icon_path,
+                expander_label=(
+                    "" if font_cache_success else text.POST_INSTALL_EXPANDER_LABEL
+                ),
+                expanded_text="" if font_cache_success else text.CACHE_NOT_UPDATED,
+            )
+            window.run()
+            window.destroy()
         else:
             log("All fonts were installed successfuly!")
+            window = aui.InfoDialogWindow(
+                title=text.WINDOW_TITLE,
+                message=text.SUCCESSFUL_INSTALL,
+                window_icon_path=self.window_icon_path,
+                expander_label=(
+                    "" if font_cache_success else text.POST_INSTALL_EXPANDER_LABEL
+                ),
+                expanded_text="" if font_cache_success else text.CACHE_NOT_UPDATED,
+            )
+            window.run()
+            window.destroy()
 
     def run(self) -> None:
         if not self.fonts_dir:
@@ -126,10 +167,26 @@ class FontsInstallerAction:
 
         files_moved = 0
         for file in self.file_paths:
-            if self.install_font_to_dir(file, self.fonts_dir):
+            if self.install_font_at_dir(file, self.fonts_dir):
                 files_moved += 1
 
-        self.finish(files_moved)
+        if files_moved == 0:
+            log("Fonts were not installed!")
+            window = aui.InfoDialogWindow(
+                title=text.WINDOW_TITLE,
+                message=text.UNSUCCESSFUL_INSTALL,
+                window_icon_path=self.window_icon_path,
+            )
+            window.run()
+            window.destroy()
+            exit(1)
+
+        font_cache_success = self.update_fonts_cache()
+
+        if not font_cache_success:
+            log("Font cache could be updated with success!")
+
+        self.finish(files_moved, font_cache_success)
 
 
 if __name__ == "__main__":
@@ -138,6 +195,6 @@ if __name__ == "__main__":
         exit(1)
 
     file_paths = [Path(path) for path in sys.argv[1:]]
-    action = FontsInstallerAction(file_paths=file_paths)
+    action = InstallFontsAction(file_paths=file_paths)
     action.run()
     exit(0)
--- install-fonts@anaximeno/files/install-fonts@anaximeno/text.py
+++ install-fonts@anaximeno/files/install-fonts@anaximeno/text.py
@@ -7,6 +7,23 @@ gettext.bindtextdomain(UUID, os.path.join(HOME, ".local/share/locale"))
 gettext.textdomain(UUID)
 
 
-def _(msg: str) -> str: return gettext.gettext(msg)
+def _(msg: str) -> str:
+    return gettext.gettext(msg)
 
 
+WINDOW_TITLE = _("Install fonts")
+
+SUCCESSFUL_INSTALL = _("Fonts were installed successfuly!")
+
+UNSUCCESSFUL_INSTALL = _("Fonts could not be installed!")
+
+PARTIAL_SUCCESS_INSTALL = _("Some fonts could not be installed!")
+
+CACHE_UPDATE_PROGRESS_MESSAGE = _("Updating fonts cache...")
+
+CACHE_NOT_UPDATED = _(
+    "Could not update font cache.\n"
+    "The installation of the fonts may not take immediate effect."
+)
+
+POST_INSTALL_EXPANDER_LABEL = _("Post installation issues")
