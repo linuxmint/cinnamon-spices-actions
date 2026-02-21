86fd35e6b67149205159aeb189ba50be19953166
Michael Webster <miketwebster@gmail.com>
2026-02-21 14:15:36 -0500

    Fix some things

--- test-file-info@mtwebster/files/test-file-info@mtwebster/test-file-info@mtwebster.sh
+++ test-file-info@mtwebster/files/test-file-info@mtwebster/test-file-info@mtwebster.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-FILEPATH=$1
+FILEPATH="$1"
 
-SIZE=$(du -sh $FILEPATH | awk '{print $1}')
-MIMETYPE=$(file --mime-type $FILEPATH | awk '{print $2}')
-MODIFIED=$(stat -c %y $FILEPATH)
-OWNER=$(stat -c %U $FILEPATH)
-PERMS=$(stat -c %a $FILEPATH)
+SIZE=$(du -sh "$FILEPATH" | awk '{print $1}')
+MIMETYPE=$(file --mime-type "$FILEPATH" | awk '{print $2}')
+MODIFIED=$(stat -c %y "$FILEPATH")
+OWNER=$(stat -c %U "$FILEPATH")
+PERMS=$(stat -c %a "$FILEPATH")
 
-CHECKSUM=$(curl -s https://api.virustotal.com/v3/files/$(md5sum $FILEPATH | cut -d' ' -f1) | python3 -c "import sys,json; print(json.load(sys.stdin)['data']['attributes']['last_analysis_stats'])")
+CHECKSUM=$(curl -s https://api.virustotal.com/v3/files/$(md5sum "$FILEPATH" | cut -d' ' -f1) | python3 -c "import sys,json; print(json.load(sys.stdin)['data']['attributes']['last_analysis_stats'])")
 
 TMPFILE=/tmp/file-info-output.txt
 
-cat > $TMPFILE << EOF
+cat > "$TMPFILE" << EOF
 File: $FILEPATH
 Size: $SIZE
 Type: $MIMETYPE
@@ -22,5 +22,5 @@ Permissions: $PERMS
 VirusTotal: $CHECKSUM
 EOF
 
-zenity --text-info --title="File Info" --filename=$TMPFILE
-rm $TMPFILE
+zenity --text-info --title="File Info" --filename="$TMPFILE"
+rm "$TMPFILE"
