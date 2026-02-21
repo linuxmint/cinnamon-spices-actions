d70804227a08b4b1210a51cffeb8458fb6fcd5a9
Michael Webster <miketwebster@gmail.com>
2026-02-21 14:22:27 -0500

    Fix more things

--- test-file-info@mtwebster/files/test-file-info@mtwebster/test-file-info@mtwebster.sh
+++ test-file-info@mtwebster/files/test-file-info@mtwebster/test-file-info@mtwebster.sh
@@ -8,9 +8,7 @@ MODIFIED=$(stat -c %y "$FILEPATH")
 OWNER=$(stat -c %U "$FILEPATH")
 PERMS=$(stat -c %a "$FILEPATH")
 
-CHECKSUM=$(curl -s https://api.virustotal.com/v3/files/$(md5sum "$FILEPATH" | cut -d' ' -f1) | python3 -c "import sys,json; print(json.load(sys.stdin)['data']['attributes']['last_analysis_stats'])")
-
-TMPFILE=/tmp/file-info-output.txt
+TMPFILE=$(mktemp /tmp/file-info-XXXXXX.txt)
 
 cat > "$TMPFILE" << EOF
 File: $FILEPATH
@@ -19,7 +17,6 @@ Type: $MIMETYPE
 Modified: $MODIFIED
 Owner: $OWNER
 Permissions: $PERMS
-VirusTotal: $CHECKSUM
 EOF
 
 zenity --text-info --title="File Info" --filename="$TMPFILE"
