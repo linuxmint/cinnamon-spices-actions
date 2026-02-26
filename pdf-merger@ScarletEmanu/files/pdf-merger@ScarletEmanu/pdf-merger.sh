c2752b4008401ae442700fdc5bdc6db94d980f6b
Loc Huynh <huynhloc.contact@gmail.com>
2026-02-27 06:50:42 +0700

    pdf-merger: correct UI text to reflect merge order (issue #617)

--- pdf-merger@ScarletEmanu/files/pdf-merger@ScarletEmanu/pdf-merger.sh
+++ pdf-merger@ScarletEmanu/files/pdf-merger@ScarletEmanu/pdf-merger.sh
@@ -10,7 +10,7 @@ dir=`dirname "$1"` ;
 # Ask for the Name of the output file
 _TITLE=$"Name the Output File"
 TITLE="$(/usr/bin/gettext "$_TITLE")"
-_TEXT=$"The input files will be united in alphabetical order into one output file.\nEnter the name of the output file here (without the extension .pdf)" 
+_TEXT=$"The input files will be merged in the order you selected into one output file.\nEnter the name of the output file here (without the extension .pdf)" 
 TEXT="$(/usr/bin/gettext "$_TEXT")"
 if ! OUTPUTNAME=$(zenity --entry \
   --title="$TITLE" \
